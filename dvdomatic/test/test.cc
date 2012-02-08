343d76953b9e3f39e3fa5092db6aea60956bcef0
Carl Hetherington <cth@carlh.net>
2012-02-08 18:45:06 +0000

    DCP naming stuff.

--- dvdomatic/test/test.cc
+++ dvdomatic/test/test.cc
@@ -1,5 +1,6 @@
 #include "format.h"
 #include "film.h"
+#include "dcp_name.h"
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE dvdomatic_test
 #include <boost/test/unit_test.hpp>
@@ -25,3 +26,15 @@ BOOST_AUTO_TEST_CASE (format_test)
 	BOOST_CHECK_EQUAL (f->ratio_as_integer(), 239);
 }
 
+BOOST_AUTO_TEST_CASE (dcp_name_test)
+{
+	string n;
+
+	n = dcp_name ("THE-BLUES-BROS", DCP::ContentType (DCP::ContentType::FEATURE), DCP::FLAT, DCP::FIFTEEN, DCP::MONO);
+	BOOST_CHECK_EQUAL (n, "THE-BLUES-BROS_FTR_F_EN-XX_UK-15_10-EN_2K_ST_20120101_FAC_OV");
+
+	n = dcp_name ("LIFE-AQUATIC", DCP::ContentType (DCP::ContentType::FEATURE), DCP::SCOPE, DCP::FIFTEEN, DCP::FIVE_POINT_ONE);
+	BOOST_CHECK_EQUAL (n, "LIFE-AQUATIC_FTR_S_EN-XX_UK-15_51-EN_2K_ST_20120101_FAC_OV");
+}
+
+		      
