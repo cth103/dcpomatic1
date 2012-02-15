2cd7500321fb55d0f94563037a57320c89eda61d
Carl Hetherington <cth@carlh.net>
2012-02-15 03:01:50 +0000

    Do proper leak checking with memcheck.

--- dvdomatic/run-makedcp.sh
+++ dvdomatic/run-makedcp.sh
@@ -4,7 +4,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:build/src
 if [ "$1" == "--debug" ]; then
     gdb --args build/tools/makedcp $2
 elif [ "$1" == "--valgrind" ]; then
-    valgrind --tool="memcheck" build/tools/makedcp $2
+    valgrind --tool="memcheck" --leak-check=full --show-reachable=yes build/tools/makedcp $2
 else
     build/tools/makedcp $*
 fi
