b23447fbb31e517993ab50616e9f5df2cc31319b
Carl Hetherington <cth@carlh.net>
2012-02-23 23:54:02 +0000

    Wire it in.

--- dvdomatic/src/decoder_factory.cc
+++ dvdomatic/src/decoder_factory.cc
@@ -17,12 +17,21 @@
 
 */
 
+#include <boost/filesystem.hpp>
 #include "ffmpeg_decoder.h"
+#include "tiff_decoder.h"
+#include "film_state.h"
+
+using namespace boost;
 
 Decoder *
 decoder_factory (
-	boost::shared_ptr<const FilmState> fs, boost::shared_ptr<const Options> o, Job* j, Log* l, bool minimal = false, bool ignore_length = false
+	shared_ptr<const FilmState> fs, shared_ptr<const Options> o, Job* j, Log* l, bool minimal = false, bool ignore_length = false
 	)
 {
+	if (filesystem::is_directory (fs->file (fs->content))) {
+		return new TIFFDecoder (fs, o, j, l, minimal, ignore_length);
+	}
+
 	return new FFmpegDecoder (fs, o, j, l, minimal, ignore_length);
 }
