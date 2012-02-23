e4f5ef999654ae69dac898bbba9f60b0db2ffd7a
Carl Hetherington <cth@carlh.net>
2012-02-23 14:17:08 +0000

    Fix warning.

--- dvdomatic/src/util.cc
+++ dvdomatic/src/util.cc
@@ -206,14 +206,15 @@ opendcp_version ()
 	while (!feof (f)) {
 		char* buf = 0;
 		size_t n = 0;
-		getline (&buf, &n, f);
-		if (n > 0) {
+		ssize_t const r = getline (&buf, &n, f);
+		if (r > 0) {
 			string s (buf);
 			vector<string> b;
 			split (b, s, is_any_of (" "));
 			if (b.size() >= 3 && b[0] == "OpenDCP" && b[1] == "version") {
 				version = b[2];
 			}
+			free (buf);
 		}
 	}
 
