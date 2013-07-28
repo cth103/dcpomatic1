431040f1d475696719d1c4a3650695b523d50037
Carl Hetherington <cth@carlh.net>
2013-07-28 21:17:12 +0100

    Try to resolve deps for ffprobe.

--- platform/osx/make_dmg.sh
+++ platform/osx/make_dmg.sh
@@ -73,7 +73,7 @@ universal_copy $ENV lib/libfreetype*.dylib $WORK/$libs
 universal_copy $ENV lib/libexpat*.dylib $WORK/$libs
 universal_copy $ENV lib/libxmlsec1*.dylib $WORK/$libs
 
-for obj in $WORK/$macos/dvdomatic $WORK/$libs/*.dylib; do
+for obj in $WORK/$macos/dvdomatic $WORK/$macos/ffprobe $WORK/$libs/*.dylib; do
   deps=`otool -L $obj | awk '{print $1}' | egrep "(/Users/carl|libboost|libssh)"`
   changes=""
   for dep in $deps; do
