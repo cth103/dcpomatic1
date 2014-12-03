b4b9097568d06b1aa0b472f02ed44ffeb7af7e6f
Carl Hetherington <cth@carlh.net>
2014-12-03 21:34:11 +0000

    more debug.

--- src/lib/ffmpeg_decoder.cc
+++ src/lib/ffmpeg_decoder.cc
@@ -408,7 +408,7 @@ FFmpegDecoder::decode_audio_packet ()
 
 		int frame_finished;
 		int decode_result = avcodec_decode_audio4 (audio_codec_context(), _frame, &frame_finished, &copy_packet);
-		LOG_WARNING (String::compose ("decode_result=%1", decode_result));
+		LOG_WARNING ("decode_result=%1", decode_result);
 		if (decode_result < 0) {
 			/* avcodec_decode_audio4 can sometimes return an error even though it has decoded
 			   some valid data; for example dca_subframe_footer can return AVERROR_INVALIDDATA
@@ -426,7 +426,7 @@ FFmpegDecoder::decode_audio_packet ()
 			   we've processed this data.
 			*/
 			decode_result = copy_packet.size;
-			LOG_WARNING (String::compose ("fudged to %1", decode_result));
+			LOG_WARNING ("fudged to %1", decode_result);
 		}
 
 		if (frame_finished) {
@@ -457,7 +457,7 @@ FFmpegDecoder::decode_audio_packet ()
 				0, audio_codec_context()->channels, _frame->nb_samples, audio_sample_format (), 1
 				);
 
-			LOG_WARNING (String::compose ("got data_size=%1", data_size));
+			LOG_WARNING ("got data_size=%1", data_size);
 			audio (deinterleave_audio (_frame->data, data_size), _audio_position);
 		}
 			
