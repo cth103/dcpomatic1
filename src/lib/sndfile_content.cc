5c1cdb42ba2b182cdb9ec290780782db10248a82
Carl Hetherington <cth@carlh.net>
2015-01-06 10:39:29 +0000

    Missing use of FrameRateChange::repeat when dividing source frame rate by DCP frame rate (fixes #440).

--- src/lib/sndfile_content.cc
+++ src/lib/sndfile_content.cc
@@ -151,7 +151,7 @@ SndfileContent::full_length () const
 	FrameRateChange frc = film->active_frame_rate_change (position ());
 
 	OutputAudioFrame const len = divide_with_round (
-		audio_length() * output_audio_frame_rate() * frc.source,
+		audio_length() * output_audio_frame_rate() * frc.source * frc.repeat,
 		content_audio_frame_rate() * film->video_frame_rate()
 		);
 	
