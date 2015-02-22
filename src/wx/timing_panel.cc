21c6cf134c2e6d27f9fcfd0c6345738dcdf9d653
Carl Hetherington <cth@carlh.net>
2015-02-22 23:05:02 +0000

    Fix typo in previous.

--- src/wx/timing_panel.cc
+++ src/wx/timing_panel.cc
@@ -106,7 +106,7 @@ TimingPanel::TimingPanel (FilmEditor* e)
 	 * http://trac.wxwidgets.org/ticket/13389
 	 */
 
-	wxString in = _("<i>Only change this if it the content's frame rate has been read incorrectly.</i>");
+	wxString in = _("<i>Only change this if the content's frame rate has been read incorrectly.</i>");
 	wxString out;
 	int const width = 20;
 	int current = 0;
