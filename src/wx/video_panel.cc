83f1ea8eacb4d56860ccdb9db5acdf12c6e71dfb
Carl Hetherington <cth@carlh.net>
2015-03-07 16:54:04 +0000

    Ellipsize filters string if it is long.

--- src/wx/video_panel.cc
+++ src/wx/video_panel.cc
@@ -259,10 +259,13 @@ VideoPanel::film_content_changed (int property)
 		}
 	} else if (property == FFmpegContentProperty::FILTERS) {
 		if (fcs) {
-			string const p = Filter::ffmpeg_string (fcs->filters ());
+			string p = Filter::ffmpeg_string (fcs->filters ());
 			if (p.empty ()) {
 				_filters->SetLabel (_("None"));
 			} else {
+				if (p.length() > 14) {
+					p = p.substr (0, 14) + "...";
+				}
 				_filters->SetLabel (std_to_wx (p));
 			}
 		}
