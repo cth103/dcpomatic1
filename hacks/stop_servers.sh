0c7466a2c2fc3972c34751802f84eda29a024e56
Carl Hetherington <cth@carlh.net>
2015-05-19 09:17:04 +0100

    Tweak start/stop servers scripts.

--- hacks/stop_servers.sh
+++ hacks/stop_servers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-dsh -m cs2-1  -m cs2-2  -m cs2-3  -m cs2-4  -m cs2-5  -m cs2-6  -m cs2-7  -m cs2-8 \
+dsh -m cs2-5 -m cs2-6 -m cs2-7 -m cs2-8 -m cs2-17 -m cs2-18 -m cs2-19 -m cs2-20 \
     killall dcpomatic_server_cli
